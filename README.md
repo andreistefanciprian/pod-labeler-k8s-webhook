5ca7e4559cf168aeab08c640f4a5bd19c3c4ff75
Stefan Andrei <andreistefanciprian@gmail.com>
2023-07-27 08:17:20 +0930

    first commit

---
+++ README.md
@@ -0,0 +1,88 @@
+# Kubernetes Webhook for Modifying Pod API Requests
+
+## Description
+
+This project aims to create a Kubernetes webhook that intercepts CREATE Pod API requests before they reach the Kubernetes API server. The webhook then modifies these requests by adding a new label to the Pods.
+
+## Prerequisites
+
+Before getting started with the webhook, ensure that the following tools and resources are available:
+
+- **cfssl**: Required for generating TLS certificates.
+- **Docker**: The webhook runs as a container, so Docker is necessary.
+- **Kubernetes Cluster**: You'll need a running Kubernetes cluster where the webhook will be deployed.
+- **Go**: The webhook is written in Go.
+
+## Generate Certificates
+
+Before deploying the webhook, you need to generate TLS certificates using cfssl. Follow these steps:
+
+1. Generate the CA certificate:
+   ```
+   cfssl print-defaults config > infra/config.json
+   cfssl print-defaults csr > infra/csr.json
+   cfssl gencert -initca infra/csr.json | cfssljson -bare infra/ca
+   ```
+
+2. Generate TLS certificates for the webhook:
+   ```
+   cfssl gencert \
+   -ca=infra/ca.pem \
+   -ca-key=infra/ca-key.pem \
+   -config=infra/config.json \
+   -hostname="pod-labeler,pod-labeler.default.svc.cluster.local,pod-labeler.default.svc,localhost,127.0.0.1" \
+   -profile=server \
+   infra/pod-labeler-csr.json | cfssljson -bare infra/pod-labeler
+   ```
+
+## Create Manifests
+
+After generating the TLS certificates, create Kubernetes manifests for deploying the webhook:
+
+1. Create a Kubernetes Secret to store the TLS certificates:
+   ```
+   kubectl create secret tls pod-labeler-tls \
+   --key=infra/pod-labeler-key.pem \
+   --cert=infra/pod-labeler.pem \
+   --dry-run=client -o yaml >infra/secret.yaml
+   ```
+
+2. Generate the webhook configuration manifest:
+   ```
+   # Replace ${CA_PEM_B64} in the webhook_template.yaml with base64-encoded CA certificate
+   ca_pem_b64=`openssl base64 -A <infra/ca.pem`
+   sed -e 's@${CA_PEM_B64}@'$ca_pem_b64'@g' <infra/webhook_template.yaml > infra/webhook.yaml
+   ```
+
+## Build and Run the Webhook
+
+Build and deploy the webhook using the provided tasks:
+
+1. Build and push the Docker image to the container registry:
+   ```
+   task build
+   ```
+
+2. Deploy the webhook to the Kubernetes cluster:
+   ```
+   task deploy
+   ```
+
+3. Test webhook:
+   ```
+   kubectl logs -l app=pod-labeler -f
+   kubectl apply -f infra/test-pod.yaml
+   kubectl get pods --show-labels
+   ```
+
+4. To uninstall the webhook, run:
+   ```
+   task undeploy
+   kubectl delete -f infra/test-pod.yaml
+   ```
+
+Feel free to adjust the tasks and configurations as needed to fit your specific environment.
+
+## License
+
+This project is licensed under the [MIT License](LICENSE). Feel free to use and modify it according to your requirements.
---
+++ main.go
@@ -0,0 +1,169 @@
+package main
+
+import (
+	"encoding/json"
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"strconv"
+
+	"github.com/gorilla/mux"
+	"k8s.io/api/admission/v1beta1"
+	apiv1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/runtime/serializer"
+)
+
+var (
+	universalDeserializer = serializer.NewCodecFactory(runtime.NewScheme()).UniversalDeserializer()
+)
+
+func main() {
+	// Parse CLI params
+	parameters := parseFlags()
+
+	// Create a new http server
+	httpMux := mux.NewRouter()
+	httpMux.HandleFunc("/healthz", HandleHealthz)
+	httpAddr := ":" + strconv.Itoa(parameters.httpPort)
+	httpServer := http.Server{
+		Addr:    httpAddr,
+		Handler: httpMux,
+	}
+
+	// Start the http server in a separate goroutine
+	go func() {
+		log.Printf("Starting http Server on port %s", httpAddr)
+		err := httpServer.ListenAndServe()
+		if err != nil {
+			log.Fatal(err)
+		}
+	}()
+
+	// Create a new https server
+	httpsMux := mux.NewRouter()
+	httpsMux.HandleFunc("/mutate", HandleMutate)
+	httpsAddr := ":" + strconv.Itoa(parameters.httpsPort)
+	httpsServer := http.Server{
+		Addr:    httpsAddr,
+		Handler: httpsMux,
+	}
+
+	// Start the https server in a separate goroutine
+	go func() {
+		log.Printf("Starting https Server on port %s", httpsAddr)
+
+		err := httpsServer.ListenAndServeTLS(parameters.certFile, parameters.keyFile)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}()
+
+	// Use a channel to block the main goroutine and keep the program running
+	select {}
+}
+
+// ServerParameters struct holds the parameters for the webhook server.
+type ServerParameters struct {
+	httpsPort int    // https server port
+	httpPort  int    // http server port
+	certFile  string // path to the x509 certificate for https
+	keyFile   string // path to the x509 private key matching `CertFile`
+}
+
+// patchOperation struct represents a JSON patch operation used in mutating Kubernetes resources.
+type patchOperation struct {
+	Op    string      `json:"op"`
+	Path  string      `json:"path"`
+	Value interface{} `json:"value,omitempty"`
+}
+
+func parseFlags() ServerParameters {
+	var parameters ServerParameters
+
+	// Define and parse CLI params using the "flag" package.
+	flag.IntVar(&parameters.httpPort, "httpPort", 8080, " Http server port (healthcheck endpoint).")
+	flag.IntVar(&parameters.httpsPort, "httpsPort", 443, " Https server port (webhook endpoint).")
+	flag.StringVar(&parameters.certFile, "tlsCertFile", "/etc/webhook/certs/tls.crt", "File containing the x509 Certificate for HTTPS.")
+	flag.StringVar(&parameters.keyFile, "tlsKeyFile", "/etc/webhook/certs/tls.key", "File containing the x509 private key to --tlsCertFile.")
+	flag.Parse()
+
+	return parameters
+}
+
+// HandleHealthz is a liveness probe.
+func HandleHealthz(w http.ResponseWriter, r *http.Request) {
+	log.Printf("Health check at %v\n", r.URL.Path)
+	w.WriteHeader(http.StatusOK)
+}
+
+// HandleMutate is the HTTP handler function for the /mutate endpoint.
+func HandleMutate(w http.ResponseWriter, r *http.Request) {
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		http.Error(w, "Failed to read request body", http.StatusInternalServerError)
+		return
+	}
+
+	var admissionReviewReq v1beta1.AdmissionReview
+	if _, _, err := universalDeserializer.Decode(body, nil, &admissionReviewReq); err != nil {
+		http.Error(w, fmt.Sprintf("Could not deserialize request: %s\n", err.Error()), http.StatusBadRequest)
+		return
+	} else if admissionReviewReq.Request == nil {
+		http.Error(w, "Malformed admission review (request is nil)", http.StatusBadRequest)
+		return
+	}
+
+	log.Printf("New Admission Review Request is being processed: User: %v \t Type: %v \t Event: %v \t Name: %v \n",
+		admissionReviewReq.Request.UserInfo.Username,
+		admissionReviewReq.Request.Kind,
+		admissionReviewReq.Request.Operation,
+		admissionReviewReq.Request.Name,
+	)
+
+	var pod apiv1.Pod
+	err = json.Unmarshal(admissionReviewReq.Request.Object.Raw, &pod)
+	if err != nil {
+		http.Error(w, fmt.Sprintf("Could not unmarshal pod on admission request: %s\n", err.Error()), http.StatusInternalServerError)
+		return
+	}
+
+	var patches []patchOperation
+	labels := pod.ObjectMeta.Labels
+	labels["new-label"] = "from-webhook"
+	patches = append(patches, patchOperation{
+		Op:    "add",
+		Path:  "/metadata/labels",
+		Value: labels,
+	})
+
+	patchBytes, err := json.Marshal(patches)
+	if err != nil {
+		http.Error(w, fmt.Sprintf("Could not marshal JSON patch: %s\n", err.Error()), http.StatusInternalServerError)
+		return
+	}
+
+	admissionReviewResponse := v1beta1.AdmissionReview{
+		Response: &v1beta1.AdmissionResponse{
+			UID:     admissionReviewReq.Request.UID,
+			Allowed: true,
+		},
+	}
+
+	admissionReviewResponse.Response.Patch = patchBytes
+
+	bytes, err := json.Marshal(&admissionReviewResponse)
+	if err != nil {
+		http.Error(w, fmt.Sprintf("Could not marshal JSON Admission Response: %s\n", err.Error()), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	log.Printf("Updated labels for Pod %v: %v \n",
+		admissionReviewReq.Request.Name,
+		labels,
+	)
+	w.Write(bytes)
+}
